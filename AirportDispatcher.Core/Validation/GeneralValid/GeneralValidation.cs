7bdaaf7e90669c2a13641b4bf6106868aa5e256f
Seqyuvi <evgeniikirik@gmail.com>
2025-01-27 23:37:57 +0500

    Add Validation Classes

---
+++ AirportDispatcher.Core/Validation/GeneralValid/GeneralValidation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportDispatcher.Core.Validation.GeneralValid
+{
+    public class GeneralValidation
+    {
+    }
+}
